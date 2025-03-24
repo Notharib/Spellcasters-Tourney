979b9ed5ad8a0e450bfa5840648e71c212388e8e
Notharib <60750169+Notharib@users.noreply.github.com>
2025-03-24 12:28:49 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-import socket, json, threading, pygame, random
+import socket, json, threading, random, time
 
 class Server:
     def __init__(self):
         self.__HOST = '127.0.0.1'
         self.__PORT = 50000
         self.__clientList = []
-        self.__spawnPoints = [(200,200), (300,300),(250,250)]
+        self.__spawnPoints = [(250,250), (350,350),(450,450)]
 
 
     def start(self):
@@ -19,9 +19,11 @@ class Server:
                 self.__clientList.append(conn)
                 colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
                 position = random.choice(self.__spawnPoints)
-                clientNoMessage = json.dumps({"type":"clientNo","data":{"clientNo":len(self.__clientList)+1,"colour": colour, "position":position}})
+                clientNoMessage = json.dumps({"type":"clientNo","data":{"clientNo":len(self.__clientList)+1,"colourTuple": colour, "positionList":position}})
                 print(clientNoMessage)
                 conn.send(clientNoMessage.encode())
+                time.sleep(0.1)
+                self.createStage(conn)
                 threading.Thread(target=self.recv_from_client, args=(conn,)).start()
 
     def notifyClientsOfConn(self,connection,colour,position):
@@ -29,6 +31,17 @@ class Server:
             if client != connection:
                 message = json.dumps({"type":"playerJoin","data":{"clientNo":len(self.__clientList)+1, "colour": colour, "position":position}})
                 client.send(message.encode())
+
+
+    def createStage(self,connection):
+        platformSize = [20,500]
+        platformPositions =[[300,200],[200,300],[500,200]]
+        for position in platformPositions:
+            positionX = position[0]
+            positionY = position[1]
+            positionMessage = json.dumps({"type":"createPlat", "data": {"positionX":positionX, "positionY":positionY, "sizeHeight":platformSize[0],"sizeWidth":platformSize[1]}})
+            connection.send(positionMessage.encode())
+            time.sleep(0.2)
 
     def recv_from_client(self, conn):
         while True:
