998890f3e1123d692412691d22c915e65efc7de8
Notharib <60750169+Notharib@users.noreply.github.com>
2025-03-24 15:05:15 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,18 @@
 import socket, json, threading, random, time
 
+class Client:
+    def __init__(self, position,colour,cl,clientNo):
+        self.client = cl
+        self.position = position
+        self.colour = colour
+        self.clientNo = clientNo
+
 class Server:
     def __init__(self):
         self.__HOST = '127.0.0.1'
         self.__PORT = 50000
         self.__clientList = []
-        self.__spawnPoints = [(250,250), (350,350),(450,450)]
+        self.__spawnPoints = [[250,250], [350,350],[450,450]]
 
 
     def start(self):
@@ -16,22 +23,33 @@ class Server:
             while True:
                 conn, addr = s.accept()
                 print("New Connection from ", addr)
-                self.__clientList.append(conn)
+
                 colour = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
                 position = random.choice(self.__spawnPoints)
                 clientNoMessage = json.dumps({"type":"clientNo","data":{"clientNo":len(self.__clientList)+1,"colourTuple": colour, "positionList":position}})
                 print(clientNoMessage)
                 conn.send(clientNoMessage.encode())
                 time.sleep(0.1)
+
+                if len(self.__clientList) != 0:
+                    self.createAlreadyJoinedPlayers(conn)
+
+                self.__clientList.append(Client(position,colour,conn,len(self.__clientList)+1))
+                time.sleep(0.1)
                 self.createStage(conn)
                 threading.Thread(target=self.recv_from_client, args=(conn,)).start()
 
     def notifyClientsOfConn(self,connection,colour,position):
         for client in self.__clientList:
-            if client != connection:
-                message = json.dumps({"type":"playerJoin","data":{"clientNo":len(self.__clientList)+1, "colour": colour, "position":position}})
-                client.send(message.encode())
+            if client.client != connection:
+                message = json.dumps({"type":"playerJoin","data":{"clientNo":len(self.__clientList)+1, "colourTuple": colour, "positionList":position}})
+                client.client.send(message.encode())
 
+    def createAlreadyJoinedPlayers(self,connection):
+        for client in self.__clientList:
+            message = json.dumps({"type": "playerJoin","data": {"clientNo": client.clientNo, "colourTuple": client.colour,"positionList": client.position}})
+            connection.send(message.encode())
+            time.sleep(0.2)
 
     def createStage(self,connection):
         platformSize = [20,500]
@@ -49,10 +67,25 @@ class Server:
             if not data:
                 break
             else:
-                print(data.decode())
-                for client in self.__clientList:
-                    if client != conn:
-                        client.send(data)
+                try:
+                    message = dict(json.loads(data.decode()))
+                    if message["type"] == "movement":
+
+                        for client in self.__clientList:
+                            if client.client == conn:
+                                clPos = client.clientNo-1
+
+
+                        if message["data"]["direction"] == "y":
+                            self.__clientList[clPos].position[1] = message["data"]["movedTo"]
+                        else:
+                            self.__clientList[clPos].position[0] = message["data"]["movedTo"]
+                    for client in self.__clientList:
+                        if client.client != conn:
+                            client.client.send(data)
+                except json.JSONDecodeError as err:
+                    print(data.decode())
+                    print("JSON Syntax Error:", err)
 
 
 if __name__ == '__main__':
